ecedea3be78acf376d72714b86f0e4981733507c
nazinamu66 <nazinamu66@yahoo.com>
2025-04-08 16:55:09 +0100

    Added first model and admin views also created the first product lists template

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inventory import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('products/', views.product_list, name='product_list'),
 ]
--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'last_updated')
+    search_fields = ('name', 'description')
+    list_filter = ('last_updated',)
+    ordering = ('-last_updated',)
